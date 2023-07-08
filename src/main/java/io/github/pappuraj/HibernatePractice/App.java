16f61bbb023e625c6f5e1174477a50a28c67f0ad
PAPPURAJ <pappuraj.duet@gmail.com>
2023-07-08 16:38:12 +0600

    'setup'

---
+++ src/main/java/io/github/pappuraj/HibernatePractice/App.java
@@ -0,0 +1,23 @@
+package io.github.pappuraj.HibernatePractice;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+
+    	Configuration cfg=new Configuration();
+    	cfg.configure("hibernate.cfg.xml");
+    	SessionFactory factory=cfg.buildSessionFactory();
+    	
+    	
+    	
+        System.out.println( factory.isOpen() );
+    }
+}
